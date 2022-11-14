72e81763f3c4120563b76a09675530b109ac84c4
Mihai Negru <determinant289@gmail.com>
2022-11-15 00:44:54 +0200

    Make game class a singleton class to manage a single instance of the game

--- src/game/GwentStone.java
+++ src/game/GwentStone.java
@@ -1,4 +1,34 @@
 package game;
 
-public class GwentStone {
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import fileio.Input;
+
+public final class GwentStone {
+
+    private GwentStone() {
+    }
+
+    private static class GwentStoneHelper {
+        private static final GwentStone GWENT_STONE_GAME = new GwentStone();
+    }
+
+    public static GwentStone getGame() {
+        return GwentStoneHelper.GWENT_STONE_GAME;
+    }
+
+    /**
+     *
+     * @param gameInput
+     * @param gameOutput
+     */
+    public void start(final Input gameInput, final ArrayNode gameOutput) {
+        game(gameOutput);
+        stop(gameOutput);
+    }
+
+    private void game(final ArrayNode gameOutput) {
+    }
+
+    private void stop(final ArrayNode gameOutput) {
+    }
 }
